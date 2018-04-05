c4b2f1cca22f282e851bd3d252435a8ceac60280
Nishant Patel <nishantuwindsor@gmail.com>
2018-04-04 21:17:55 -0400

    Use ScaleGestureDector to perform pinch zoom

--- app/src/main/java/com/stepin2it/ui/activity/ProductImageActivity.java
+++ app/src/main/java/com/stepin2it/ui/activity/ProductImageActivity.java
@@ -3,6 +3,8 @@ package com.stepin2it.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -16,6 +18,9 @@ public class ProductImageActivity extends AppCompatActivity {
 
     @BindView(R.id.imv_product_image_large)
     ImageView imvProductImageView;
+
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mScaleFactor = 1.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +46,28 @@ public class ProductImageActivity extends AppCompatActivity {
             Glide.with(ProductImageActivity.this)
                     .load(intent.getStringExtra(IConstants.KEY_PRODUCT_IMAGE_URL))
                     .into(imvProductImageView);
+            mScaleGestureDetector =
+                    new ScaleGestureDetector(ProductImageActivity.this, new ScaleListener());
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mScaleGestureDetector.onTouchEvent(event);
+        return true;
+    }
+
+    private class ScaleListener
+            extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScaleFactor = mScaleFactor * detector.getScaleFactor();
+            mScaleFactor = Math.max(0.5f,
+                    Math.min(mScaleFactor, 3.0f));
+            imvProductImageView.setScaleX(mScaleFactor);
+            imvProductImageView.setScaleY(mScaleFactor);
+            return true;
         }
     }
 }
