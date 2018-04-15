f715dd109c9c13e5052ac79ac26ccf6a049c8d8c
Nishant Patel <nishantuwindsor@gmail.com>
2018-04-15 12:08:59 -0400

    Add fabric crashlytics

--- app/src/main/java/com/stepin2it/Stepin2ItApplication.java
+++ app/src/main/java/com/stepin2it/Stepin2ItApplication.java
@@ -2,8 +2,10 @@ package com.stepin2it;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.facebook.stetho.Stetho;
 
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 public class Stepin2ItApplication extends Application {
@@ -11,6 +13,11 @@ public class Stepin2ItApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(Stepin2ItApplication.this);
+        final Fabric fabric = new Fabric.Builder(this)
+                .kits(new Crashlytics())
+                .debuggable(true)
+                .build();
+        Fabric.with(fabric);
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
